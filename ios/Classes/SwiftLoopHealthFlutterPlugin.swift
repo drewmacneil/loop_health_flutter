f0c760d1c4f764667561d7b7550378ec1d3ae41b
Drew MacNeil <drewm@hey.com>
2021-06-15 23:22:12 -0700

    Add dependency on LoopKit pod.

--- ios/Classes/SwiftLoopHealthFlutterPlugin.swift
+++ ios/Classes/SwiftLoopHealthFlutterPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import LoopKit
 
 public class SwiftLoopHealthFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
